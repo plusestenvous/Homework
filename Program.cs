8a46016d8fcf3c5433cd9d5f1d80854462f1d961
Arzina <delilaharzinadragon@mail.ru>
2022-12-12 15:59:31 +0300

    commited

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿/* Напишите цикл, который принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B. */
+
+int Extent (int nA, int nB) {
+  int result = 1;
+  for(int i = 1; i <= nB; i++){
+    result = result * nA;
+  }
+    return result;
+}
+  Console.Write("Введите число A: ");
+  int nA = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Введите число B: ");
+  int nB = Convert.ToInt32(Console.ReadLine());
+
+  int extent = Extent(nA, nB);
+  Console.WriteLine("Ответ: " + extent);
+
+
+/* Напишите программу, которая принимает на вход число 
+и выдаёт сумму цифр в числе. */
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+  int Summa(int number) {   
+    int count = Convert.ToString(number).Length;
+    int sum = 0;
+    int result = 0;
+    for (int i = 0; i < count; i++) {
+      sum = number - number % 10;
+      result = result + (number - sum);
+      number = number / 10;
+    }
+   return result;
+  }
+int numberSumma = Summa(number);
+Console.WriteLine("Сумма цифр: " + numberSumma);
+
+
+/* Напишите программу, которая задаёт массив из 
+8 элементов и выводит их на экран. */
+
+Console.Write("Числа: ");
+string? Numbers = Console.ReadLine();
+for (int i = 0; i < numbers.Length; i++)
+  {
+    if (numbers[i] != ' ') 
+    {
+      numbersMassive += numbers[i];
+    }
+  }
+  return numbersMassive;
+}
