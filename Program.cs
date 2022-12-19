0c3c058386c42839d02bf5211533667c575535d9
Arzina <delilaharzinadragon@mail.ru>
2022-12-19 13:15:49 +0300

    commited

--- Program.cs
+++ Program.cs
@@ -1,43 +1,76 @@
-﻿/* Напишите цикл, который принимает на вход два числа (A и B) 
-и возводит число A в натуральную степень B. */
+﻿/* Задайте массив заполненный случайными положительными трёхзначными числами.
+Напишите программу, которая покажет количество чётных чисел в массиве.*/
 
-int Extent (int nA, int nB) {
-  int result = 1;
-  for(int i = 1; i <= nB; i++){
-    result = result * nA;
-  }
-    return result;
-}
-  Console.Write("Введите число A: ");
-  int nA = Convert.ToInt32(Console.ReadLine());
-  Console.Write("Введите число B: ");
-  int nB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+ArrayRandomNumbers(numbers);
+Console.WriteLine("Массив: ");
+PrintArray(numbers);
+int count = 0;
 
-  int extent = Extent(nA, nB);
-  Console.WriteLine("Ответ: " + extent);
+for (int a = 0; a < numbers.Length; a++)
+if (numbers[a] % 2 == 0)
+count++;
 
+Console.WriteLine($"чисел: {numbers.Length}, четные: {count}");
 
-/* Напишите программу, которая принимает на вход число 
-и выдаёт сумму цифр в числе. */
-
-Console.Write("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-  int Summa(int number) {   
-    int count = Convert.ToString(number).Length;
-    int sum = 0;
-    int result = 0;
-    for (int i = 0; i < count; i++) {
-      sum = number - number % 10;
-      result = result + (number - sum);
-      number = number / 10;
+void ArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(10,100);
     }
-   return result;
-  }
-int numberSumma = Summa(number);
-Console.WriteLine("Сумма цифр: " + numberSumma);
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+/* Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях. */
+
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32((Console.ReadLine()));
+int[] array = new int[size];
+Random random = new Random();
+for (int i = 0; i < size; i++)
+{
+    array[i] = random.Next(10, 100);
+    Console.Write("{0} {1}", array[i], "  ");
+}
+int Summa = 0;
+for (int i = 0; i < array.Length; i++)
+  if (array[i]%1 == 0)
+    Summa += array[i];
+  else
+  break;
+Console.Write("Сумма = : " + Summa);
+Console.ReadKey();
 
 
-/* Напишите программу, которая задаёт массив из 
-8 элементов и выводит их на экран. */
+/* Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементов массива. */
 
-// Не выполнено, не поняла задание :(
+Console.Write("Задайте массив: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] mass  = new int[size];
+Random random = new Random(); 
+for( int i = 0; i < size; i++ ) {
+  mass[i] = random.Next(-10, 100);
+  Console.Write($" {mass[i]} ");
+}
+int min = 0;
+int max = 0;
+foreach ( int i in mass ) {
+  if ( min > i ) min = i;
+  if ( max < i ) max = i;
+}
+Console.WriteLine($"\n Максимальный элемент: {max}\n Минимальный элемент: {min}");
+Console.WriteLine($"Разница между максимальным и минимальным элементом: {max-min}");
