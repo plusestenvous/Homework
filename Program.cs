a80d1d0910ca4ae70fb0ebef5512c49dfeccf220
Arzina <delilaharzinadragon@mail.ru>
2022-12-22 22:27:34 +0300

    commited

--- Program.cs
+++ Program.cs
@@ -11,4 +11,22 @@ for (int i = 0; i < arr.Length; i++)
         count++;
     }
 }
-Console.WriteLine($"Колличество элементов > 0: {count}");
+Console.WriteLine($"Колличество чисел > 0: {count}");
+
+/* Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.*/
+
+Console.WriteLine("Введите b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = ( -b2 + b1 ) / ( -k1 + k2 );
+double y = k2 * x + b2;
+
+Console.WriteLine($"Две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
