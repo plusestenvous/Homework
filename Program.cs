839e09069bec420b8fb77fc04f23876cd86d7373
Arzina <delilaharzinadragon@mail.ru>
2022-12-25 14:56:10 +0300

    commited

--- Program.cs
+++ Program.cs
@@ -1,32 +1,35 @@
-﻿/* Пользователь вводит с клавиатуры M чисел. 
-Посчитайте, сколько чисел больше 0 ввёл пользователь.*/
+﻿/* Задайте двумерный массив размером m×n, 
+заполненный случайными вещественными числами.*/
 
-Console.Write("Введите числа: ");
-int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-int count = 0;
-for (int i = 0; i < arr.Length; i++)
+Console.WriteLine("Введите количество строк:");
+int lines = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов:");
+int columns = Convert.ToInt32(Console.ReadLine());
+double[,] numbers = new double[lines, columns];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(double[,] array)
 {
-    if (arr[i] > 0)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        count++;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
     }
 }
-Console.WriteLine($"Колличество чисел > 0: {count}");
 
-/* Напишите программу, которая найдёт точку пересечения двух прямых, 
-заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
-значения b1, k1, b2 и k2 задаются пользователем.*/
-
-Console.WriteLine("Введите b1");
-double b1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите k1");
-double k1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите b2");
-double b2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите k2");
-double k2 = Convert.ToInt32(Console.ReadLine());
-
-double x = ( -b2 + b1 ) / ( -k1 + k2 );
-double y = k2 * x + b2;
-
-Console.WriteLine($"Две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
